b6acff85ec11ce52366bcd21bba46e3612eeee7a
ritacassiasr@gmail.com <ritacassiasr@gmail.com>
2023-10-14 14:50:22 -0400

    iniciando testes

---
+++ tests/integration/people.test.js
@@ -0,0 +1,30 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http"); // simular uma request a nossa API, ou seja, criar seu próprio listen()
+const sinon = require("sinon"); // cria os dubles (stubs) para a função de leitura 'fs'
+
+const app = require("../../src/app");
+const connection = require("../../src/db/connection");
+
+const { expect, use } = chai;
+
+use(chaiHttp);
+
+describe("Testando os endpoints de people", function () {
+  it("Testando o cadastro de uma pessoa ", async function () {
+    sinon.stub(connection, "execute").resolves([{ insertId: 42 }]);
+
+    const response = await chai.request(app).post("/people").send({
+      firstName: "Luke",
+      lastName: "Skywalker",
+      email: "luke.skywalker@trybe.com",
+      phone: "851 678 4453",
+    });
+
+    expect(response.status).to.equal(201);
+    expect(response.body).to.deep.equal({
+      message: "Pessoa cadastrada com sucesso com o id 42",
+    });
+  });
+
+  afterEach(sinon.restore);
+});
